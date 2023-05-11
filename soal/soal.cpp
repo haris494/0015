d6023cf7a6b1fdbdff9cd2530dbe7e8e3602664a
haris494 <hariis.459@gmail.com>
2023-05-11 10:45:08 +0700

    Add project files.

---
+++ soal/soal.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+int i;
+
+void input()
+{
+	while (true)
+	{
+		cout << "enter the lowerboard + upperboard: ";
+		cin >> n;
+		if ((n > 1) && (n <= 10))
+			break;
+		else
+			cout << 
+
+		
+
+
+
+
